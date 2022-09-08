2b84fb115f9ac3dc4daabf106cd0d0a207eb8db1
will <will2346@live.co.uk>
2022-09-08 21:27:28 +0100

    refactor

--- python/server.py
+++ python/server.py
@@ -1,8 +1,9 @@
 # reference: https://uk.mathworks.com/matlabcentral/answers/800901-interface-between-python-and-matlab-using-tcpserver
 import socket
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-    sock.connect(('localhost', 1234))
-    print("Connection to server established!")
-    sock.sendall(b'Hello World')
-    data = sock.recv(1024)
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.connect(('localhost', 1234))
+print("Connection to server established!")
+sock.sendall(b'Hello World')
+data = sock.recv(1024)
+sock.close()
 print('Received', repr(data))
